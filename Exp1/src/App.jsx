b6cf373e8b79852f3bc5972112e2aff3041d33da
Ankush <amudgil2006@gmail.com>
2026-01-27 15:32:43 +0530

    Updated

---
+++ Exp1/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import SinglePageApp from './components/Spa'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <SinglePageApp/>
+       Ankush
+     
+    </>
+  )
+}
+
+export default App
